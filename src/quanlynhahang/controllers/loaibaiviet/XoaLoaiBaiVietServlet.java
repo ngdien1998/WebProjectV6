ace24c35619285ea5522ab256d6b1dcd562eb173
PhamGiaThinh <giathinh_pham98@yahoo.com>
2018-11-14 10:32:16 +0700

    thinh commit

--- src/quanlynhahang/controllers/loaibaiviet/XoaLoaiBaiVietServlet.java
+++ src/quanlynhahang/controllers/loaibaiviet/XoaLoaiBaiVietServlet.java
@@ -3,6 +3,7 @@ package quanlynhahang.controllers.loaibaiviet;
 import quanlynhahang.models.businessmodels.LoaiBaiVietService;
 import quanlynhahang.models.datamodels.LoaiBaiViet;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,6 +18,8 @@ public class XoaLoaiBaiVietServlet extends HttpServlet {
         request.setCharacterEncoding("utf-8");
         response.setContentType("text/html;charset=UTF-8");
         try {
+            LoaiBaiViet loaiBaiViet=new LoaiBaiViet();
+            loaiBaiViet.setIdLoaiBaiViet(Integer.parseInt(request.getParameter("txtIdLoai")));
             String id = request.getParameter("txtIdLoai");
             if (id == null || id.trim().equals("")) {
                 response.setStatus(400);
@@ -32,6 +35,23 @@ public class XoaLoaiBaiVietServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        try{
+            String id=request.getParameter("id");
+            if(id==null || id.trim().equals("")){
+                response.setStatus(400);
+                return;
+            }
+            LoaiBaiVietService service=new LoaiBaiVietService();
+            LoaiBaiViet loaiBaiViet=service.get(Integer.parseInt(id));
+            if(loaiBaiViet==null ){
+                response.setStatus(404);
+                return;
+            }
+            request.setAttribute("loaiBaiViet1",loaiBaiViet);
+        }catch(SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        RequestDispatcher dispatcher=getServletContext().getRequestDispatcher("/admin-xoa-loai-bai-viet.jsp");
+        dispatcher.forward(request,response);
     }
 }
